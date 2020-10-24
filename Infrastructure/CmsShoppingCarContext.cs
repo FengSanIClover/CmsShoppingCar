656aca7732dbc4ab190ab97a77f68a905719d632
cloverfeng <clover.feng@tpisoftware.com>
2020-10-24 12:13:41 +0800

    安裝套件 Microsoft.EntityFrameworkCore.SqlServer Microsoft.EntityFrameworkCore.Tools 建立 Infrastructure 資料夾 建立 CmsShoppingCarContext Class，並繼承 DbContext 建立建構子並繼承

---
+++ Infrastructure/CmsShoppingCarContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCar.Infrastructure
+{
+    public class CmsShoppingCarContext : DbContext
+    {
+        public CmsShoppingCarContext(DbContextOptions<CmsShoppingCarContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
